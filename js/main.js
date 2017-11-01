f68d39442f37c618fdcc37e06ff7d4edbf6d8172
feng <feng@bizdev.us>
2017-11-01 11:55:50 -0600

    msTranslate working almost mvp

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-$(document).ready(function(){
-
-
-
-  //***GLOBALS
-
+//***GLOBALS
+var selection = null
+var text
+const translateAccents = {
+  yoda: "yoda.json",
+  pirate: "pirate.json",
+  piglatin: "piglatin.json",
+  dothraki: "dothraki.json",
+  huttese: "huttese.json",
+  oldenglish: "oldenglish.json",
+  shakespeare: "shakespeare.json",
+  klingon: "klingon.json",
+  jive: "jive.json",
+  cockney: "cockney.json",
+  morse: "morse.json",
+  us2uk: "us2uk.json",
+  brooklyn: "brooklyn.json",
+  australian: "australian.json",
+  boston: "boston.json",
+  austrian: "austrian.json",
+  braille: "braille.json",
+  emoji: "emoji.json"
+}
+function createSearchString (text) {
+  return encodeURIComponent(text)
+}
+function sideNav (){
   $('.button-collapse').sideNav({
     menuWidth: 300, // Default is 300
     edge: 'left', // Choose the horizontal origin
@@ -13,115 +34,108 @@ $(document).ready(function(){
     onClose: function() {
       return console.log('closed')
     }
-  });
+  })
+}
+function createNav(){
+  for (let i in translateAccents){
+    // console.log(i)
+    let groupClass = JSON.stringify("translateAccents " + i)
+    let accents = "<li class="+groupClass+"><a href=''#!'>"+i+"</a></li>"
+    $(".accent-selector").append(accents)
 
-  function createSearchString (text) {
-    console.log(text);
-    return encodeURIComponent(text)
   }
-  let selection = null
-  const translateType = {
-    yoda: "yoda.json",
-    pirate: "pirate.json",
-    piglatin: "piglatin.json",
-    dothraki: "dothraki.json",
-    huttese: "huttese.json",
-    oldenglish: "oldenglish.json",
-    shakespeare: "shakespeare.json",
-    klingon: "klingon.json",
-    jive: "jive.json",
-    cockney: "cockney.json",
-    morse: "morse.json",
-    us2uk: "us2uk.json",
-    brooklyn: "brooklyn.json",
-    australian: "australian.json",
-    boston: "boston.json",
-    austrian: "austrian.json",
-    braille: "braille.json",
-    emoji: "emoji.json"
+  for (let i in translateLanguages){
+    // console.log(i)
+    let groupClass = JSON.stringify("translateLanguages " +i)
+    let language = "<li class="+groupClass+"><a href=''#!'>"+translateLanguages[i]+"</a></li>"
+    $(".language-selector").append(language)
+
   }
-  function createNav(){
-    for (let i in translateType){
-      // console.log(i)
-      let groupClass = JSON.stringify("translateType " + i)
-      let language = "<li class="+groupClass+"><a href=''#!'>"+i+"</a></li>"
-      $(".language-selector").append(language)
+}
 
-    }
-  }
-  createNav()
-
-
-
-  // EVENT LISTENERS
-
-  // Creates event listeners for all classes in translateType
-  function createListeners(){
-    $(".submit-button").click(function(event){
-      event.preventDefault()
-      console.log('clicked')
-      if (localStorage.getItem("selected") !== null || $("#input_text")[0].innerHTML === '' ){
-        text = $("#input_text")[0]["value"]
-        console.log(text);
+// EVENT LISTENERS
+function createListeners(){
+  $(".submit-button").click(function(event){
+    event.preventDefault()
+    console.log('clicked')
+    if (localStorage.getItem("selected") !== null || $("#input_text")[0].innerHTML === '' ){
+      text = $("#input_text")[0]["value"]
+      console.log(text);
+      if (localStorage.getItem("type") === "lang"){
+        msTranslation(text)
+      }
+      else if (localStorage.getItem("type") === "accent"){
         translate(text)
       }
-      else {return alert('Please select a language.')}
-    })
-    $("#language").click(function(event){
+    }
+    else {return alert('Please select a language.')}
+  })
+  $("#language").click(function(event){
+    event.preventDefault()
+  })
+  $(".nav-head").click(function(event){
+    event.preventDefault()
+  })
+  for (let i in translateAccents){
+    let search = '.'+i
+    $(search).click(function(event){
       event.preventDefault()
+      $("#language")[0].innerHTML = "Language: "+i
+      $("#translation-title")[0].innerHTML = i
+      localStorage.setItem("selected",translateAccents[i])
+      localStorage.setItem("name",i)
+      localStorage.setItem("type","accent")
     })
-    $(".nav-head").click(function(event){
+  }
+  for (let i in translateLanguages){
+    let search = '.'+i
+    $(search).click(function(event){
       event.preventDefault()
+      $("#language")[0].innerHTML = "Language: "+translateLanguages[i]
+      $("#translation-title")[0].innerHTML = translateLanguages[i]
+      localStorage.setItem("selected",i)
+      localStorage.setItem("name",translateLanguages[i])
+      localStorage.setItem("type","lang")
     })
-    for (let i in translateType){
-      let search = '.'+i
-      $(search).click(function(event){
-        event.preventDefault()
-        $("#language")[0].innerHTML = "Language: "+i
-        $("#translation-title")[0].innerHTML = i
-        localStorage.setItem("selected",i)
-      })
-    }
   }
-  createListeners()
+}
 
-
-
-
-  //
-  function translate (text) {
-    let searchString = createSearchString(text)
-    let language = localStorage.getItem("selected")
-    console.log(language)
-    let inputString = "http://api.funtranslations.com/translate/"+language+"?text=" + searchString
-    console.log(inputString)
-    apiSearch(inputString)
+function translate (text) {
+  let searchString = createSearchString(text)
+  let language = localStorage.getItem("selected")
+  console.log(language)
+  let inputString = "http://api.funtranslations.com/translate/"+language+"?text=" + searchString
+  console.log(inputString)
+  apiSearch(inputString)
+}
+function apiSearch (inputString) {
+  let search = {
+    "method": "POST",
+    "url": inputString,
+    "dataType": "json",
   }
-  function apiSearch (inputString) {
-    let search = {
-      "method": "POST",
-      "url": inputString,
-      "dataType": "json",
-    }
-    $.ajax(search).done(function(data) {
-          let translatedString = data.contents.translated
-          console.log(translatedString)
-          $("#inputField")[0].innerHTML = translatedString
-          // return translatedString
+  $.ajax(search).done(function(data) {
+        let translatedString = data.contents.translated
+        console.log(translatedString)
+        $("#inputField")[0].innerHTML = translatedString
+        // return translatedString
 
-      }).fail(function(err){
-        console.log('Please wait an hour for the api to work. OR use a VPN.')
-      })
-      return translatedString
-    }
+    }).fail(function(err){
+      console.log('Please wait an hour for the api to work. OR use a VPN.')
+    })
+    // return translatedString
+  }
 
+
+
+
+$(document).ready(function(){
 // check to see if local storage contains a selected value when document loads
-    if (localStorage.getItem("selected") !== null){
-      $("#language")[0].innerHTML = "Language: "+localStorage.getItem("selected")
-      $("#translation-title")[0].innerHTML = localStorage.getItem("selected")
-    }
-
-
-
-
+  if (localStorage.getItem("selected") !== null){
+    $("#language")[0].innerHTML = "Language: "+localStorage.getItem("name")
+    $("#translation-title")[0].innerHTML = localStorage.getItem("name")
+  }
+  sideNav()
+  createNav()
+  createListeners()
 }) //end of document.ready
